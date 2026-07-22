361c8b7c4936ed641a44228edf430f081a19a78f
jeevankr11 <jk678580@gmail.com>
2026-07-22 17:40:54 +0530

    add updated programs

--- C-Practice/1.c
+++ C-Practice/1.c
@@ -4,8 +4,10 @@ int main ()
     int num;
     printf("Enter the number you want the multiplication table for: ");
     scanf("%d", &num);
+    printf("Multiplication table of %d is as follows : \n", num);
     for (int i = 1; i <= 10; i++)
     {
+        
         printf("%d x %d = %d\n", num, i, num * i);
     }
 
